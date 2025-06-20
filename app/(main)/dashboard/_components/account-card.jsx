2bd4731c1129365257bba52f0407fff04e2f12d2
RajwardhanSinghUmath <rajwardhansinghumath@gmail.com>
2025-06-20 12:09:57 +0530

    Change default account functionality

--- app/(main)/dashboard/_components/account-card.jsx
+++ app/(main)/dashboard/_components/account-card.jsx
@@ -2,6 +2,8 @@
 
 import { ArrowUpRight, ArrowDownRight, CreditCard } from "lucide-react";
 import { Switch } from "@/components/ui/switch";
+import { useEffect } from "react";
+import useFetch from "@/hooks/use-fetch";
 import {
   Card,
   CardContent,
@@ -10,10 +12,41 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import Link from "next/link";
+import { updateDefaultAccount } from "@/actions/account";
+import { toast } from "sonner";
 
-export default function AccountCard({ account }) {
+export function AccountCard({ account }) {
   const { name, type, balance, id, isDefault } = account;
 
+  const {
+    loading: updateDefaultLoading,
+    fn: updateDefaultFn,
+    data: updatedAccount,
+    error,
+  } = useFetch(updateDefaultAccount);
+
+  const handleDefaultChange = async (event) => {
+    event.preventDefault(); // Prevent navigation
+
+    if (isDefault) {
+      toast.warning("You need atleast 1 default account");
+      return; // Don't allow toggling off the default account
+    }
+
+    await updateDefaultFn(id);
+  };
+
+  useEffect(() => {
+    if (updatedAccount?.success) {
+      toast.success("Default account updated successfully");
+    }
+  }, [updatedAccount]);
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error.message || "Failed to update default account");
+    }
+  }, [error]);
 
   return (
     <Card className="hover:shadow-md transition-shadow group relative">
@@ -24,6 +57,8 @@ export default function AccountCard({ account }) {
           </CardTitle>
           <Switch
             checked={isDefault}
+            onClick={handleDefaultChange}
+            disabled={updateDefaultLoading}
           />
         </CardHeader>
         <CardContent>
--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,35 +1,43 @@
-import { getUserAccounts } from '@/actions/dashboard'
-import CreateAccountDrawer from '@/components/create-account-drawer'
-import { Card, CardContent } from '@/components/ui/card'
-import { Plus } from 'lucide-react'
-import React from 'react'
-import AccountCard from './_components/account-card'
 
-const DashboardPage =async () => {
+import { getUserAccounts } from "@/actions/dashboard";
+import { getDashboardData } from "@/actions/dashboard";
+import { AccountCard } from "./_components/account-card";
+import  CreateAccountDrawer  from "@/components/create-account-drawer";
+import { Card, CardContent } from "@/components/ui/card";
+import { Plus } from "lucide-react";
+export default async function DashboardPage() {
+  const [accounts, transactions] = await Promise.all([
+    getUserAccounts(),
+    getDashboardData(),
+  ]);
 
-  const accounts = await getUserAccounts()
+  const defaultAccount = accounts?.find((account) => account.isDefault);
+
+ 
+
   return (
-    <div className='px-5'>
+    <div className="space-y-8">
       {/* Budget Progress */}
-      {/* Overview */}
+ 
+
+      {/* Dashboard Overview */}
+     
 
       {/* Accounts Grid */}
-      <div className='grid gap-4 md:grid-cols-2 lg:grid-cols-1'>
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         <CreateAccountDrawer>
           <Card className="hover:shadow-md transition-shadow cursor-pointer border-dashed">
             <CardContent className="flex flex-col items-center justify-center text-muted-foreground h-full pt-5">
-              <Plus className="h-10 w-10 mb-2"/>
-              <p className='text-sm font-medium'>Add New Account</p>
+              <Plus className="h-10 w-10 mb-2" />
+              <p className="text-sm font-medium">Add New Account</p>
             </CardContent>
           </Card>
         </CreateAccountDrawer>
-
-      {  accounts.length>0 &&accounts?.map((account)=>{
-          return <AccountCard key={account.id} account={account}/>
-        })}
+        {accounts.length > 0 &&
+          accounts?.map((account) => (
+            <AccountCard key={account.id} account={account} />
+          ))}
       </div>
     </div>
-  )
+  );
 }
-
-export default DashboardPage
