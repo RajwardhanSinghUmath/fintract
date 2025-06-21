6af65002c4dc6bcd50fedba564a3a01bf6f14056
RajwardhanSinghUmath <rajwardhansinghumath@gmail.com>
2025-06-21 13:42:41 +0530

    monthly report and insights using innest and gemini api

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,13 +1,19 @@
 import { serve } from "inngest/next";
 
 import { inngest } from "@/lib/inngest/client";
-import { checkBudgetAlerts, processRecurringTransaction, triggerRecurringTransactions } from "@/lib/inngest/functions";
+import {
+  checkBudgetAlerts,
+  generateMonthlyReports,
+  processRecurringTransaction,
+  triggerRecurringTransactions,
+} from "@/lib/inngest/function";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-    checkBudgetAlerts,
+    processRecurringTransaction,
     triggerRecurringTransactions,
-    processRecurringTransaction
+    generateMonthlyReports,
+    checkBudgetAlerts,
   ],
 });
