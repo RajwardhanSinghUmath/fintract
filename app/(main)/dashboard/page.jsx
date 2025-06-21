de899905c16b3d2240a9c7199b384310126b7e28
RajwardhanSinghUmath <rajwardhansinghumath@gmail.com>
2025-06-21 13:58:52 +0530

    dashboard pie chart and last 5 tranactions

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,12 +1,14 @@
-
+import { Suspense } from "react";
 import { getUserAccounts } from "@/actions/dashboard";
 import { getDashboardData } from "@/actions/dashboard";
+import { getCurrentBudget } from "@/actions/budget";
 import { AccountCard } from "./_components/account-card";
 import  CreateAccountDrawer  from "@/components/create-account-drawer";
+import { BudgetProgress } from "./_components/budget-progress";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
-import { getCurrentBudget } from "@/actions/budget";
-import { BudgetProgress } from "./_components/budget-progress";
+import { DashboardOverview } from "./_components/transaction-overview";
+
 export default async function DashboardPage() {
   const [accounts, transactions] = await Promise.all([
     getUserAccounts(),
@@ -14,24 +16,26 @@ export default async function DashboardPage() {
   ]);
 
   const defaultAccount = accounts?.find((account) => account.isDefault);
-  let budgetData=null;
-  if(defaultAccount){
-    budgetData=await getCurrentBudget(defaultAccount.id)
+
+  // Get budget for default account
+  let budgetData = null;
+  if (defaultAccount) {
+    budgetData = await getCurrentBudget(defaultAccount.id);
   }
- 
 
   return (
     <div className="space-y-8">
       {/* Budget Progress */}
-      {defaultAccount&&(
-        <BudgetProgress
+      <BudgetProgress
         initialBudget={budgetData?.budget}
-        currenExpenses={budgetData?.currentExpenses || 0}
-        />
-      )}
+        currentExpenses={budgetData?.currentExpenses || 0}
+      />
 
       {/* Dashboard Overview */}
-     
+      <DashboardOverview
+        accounts={accounts}
+        transactions={transactions || []}
+      />
 
       {/* Accounts Grid */}
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
